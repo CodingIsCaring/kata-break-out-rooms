051a607ffcf26dd7add628d0be18d8b4ef26a109
Maria Soria <maria.it3@gmail.com>
2021-06-14 18:27:12 +0100

    green test_one_round_three_rooms

--- break_out_rooms.py
+++ break_out_rooms.py
@@ -1,10 +1,13 @@
 
 
 def break_out_rooms(rounds, participants, room_size):
+    total_rooms = len(participants) / room_size
+
     if len(participants) == room_size:
         result = [participants[0] + ", " + participants[1]]
     else:
-        result = [participants[0] + ", " + participants[1]]
-        result2 = participants[2] + ", " + participants[3]
-        result.append(result2)
+        result = []
+        for index in range(0, len(participants), 2):
+            result.append(participants[index] + ", " + participants[index + 1])
+
     return [result]
--- test_break_out_rooms.py
+++ test_break_out_rooms.py
@@ -8,3 +8,8 @@ class Test(TestCase):
 
     def test_one_round_two_rooms(self):
         self.assertEqual([["Yodra, Maria", "Ana, Monica"]], break_out_rooms(1, ["Yodra", "Maria", "Ana", "Monica"], 2))
+
+    def test_one_round_three_rooms(self):
+        self.assertEqual([["Yodra, Maria", "Ana, Monica", "Lucia, Sara"]], break_out_rooms(1, ["Yodra", "Maria",
+                                                                                               "Ana", "Monica",
+                                                                                               "Lucia", "Sara"], 2))
