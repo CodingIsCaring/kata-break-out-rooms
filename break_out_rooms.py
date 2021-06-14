f4050452325561a6578218c6017fe7af1ab87b13
Maria Soria <maria.it3@gmail.com>
2021-06-14 17:47:16 +0100

    green test_one_round_two_rooms

--- break_out_rooms.py
+++ break_out_rooms.py
@@ -1,3 +1,12 @@
-
 def sum(number_one, number_two):
     return number_one + number_two
+
+
+def break_out_rooms(rounds, participants, room_size):
+    if len(participants) == room_size:
+        result = [participants[0] + ", " + participants[1]]
+    else:
+        result = [participants[0] + ", " + participants[1]]
+        result2 = participants[2] + ", " + participants[3]
+        result.append(result2)
+    return [result]
--- test_break_out_rooms.py
+++ test_break_out_rooms.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
-from break_out_rooms import sum
+from break_out_rooms import sum, break_out_rooms
 
 
 class Test(TestCase):
     def test_sum(self):
         self.assertEqual(4, sum(2, 2))
+
+    def test_one_round(self):
+        self.assertEqual([["Yodra, Maria"]], break_out_rooms(1, ["Yodra", "Maria"], 2))
+
+    def test_one_round_two_rooms(self):
+        self.assertEqual([["Yodra, Maria", "Ana, Monica"]], break_out_rooms(1, ["Yodra", "Maria", "Ana", "Monica"], 2))
